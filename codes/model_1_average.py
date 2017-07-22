f730bb6ce706d4e1c5ee4c008f06d3ec61a4312e
Louis Yang <louis.yang.925@gmail.com>
2017-07-21 17:37:13 -0700

    v0.2.2

---
+++ codes/model_1_average.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 21 16:40:32 2017
+
+@author: Louis
+"""
+print('')
+print('Kaggle - Web Traffic Time Series Forecasting')
+print('         by Sky Huang and Louis Yang')
+print('Model 01 - Average')
+
+from read_data import read_data
+print('Reading data...', end = '')
+page, visit = read_data()
+print('done!')
+
+number_days = len(visit[0])
+number_pages = len(page)
+print('%%%%% Overview of the data: %%%%%')
+print('  Number of pages:', number_pages)
+print('  Number of days of data:', number_days)
+print('  visit[0]:', visit[0])
+print('  page[:5]', page[:5])
+
+visit[-5:]
--- codes/read_data.py
+++ codes/read_data.py
@@ -1,10 +1,10 @@
 # Importing libraries
-import numpy as np
-import matplotlib.pyplot as plt
+#import numpy as np
+#import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-read_data():
+def read_data():
     readin = pd.read_csv('../data/train_1.csv')
     page= readin.iloc[:,0].values
     visit= readin.iloc[:,1:].values
