162614b2673f91324fe8726d0f03067bd283d240
Louis Yang <louis.yang.925@gmail.com>
2017-09-09 03:05:25 -0700

    v9.4.1 Ensemble

---
+++ codes/tools.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Visualization
+def view(x, xlim=None, ylim=None, yscale='linear', title=None, show=True):
+    plt.yscale(yscale)
+    plt.plot(x)
+    if ylim: plt.ylim(ylim)
+    if xlim: plt.xlim(xlim)
+    if title: plt.title(title)
+    if show: plt.show()
+        
+def viewi(X, i, xlim=None, ylim=None, yscale='linear', show=True):
+    view(X[i], xlim=xlim, ylim=ylim, yscale=yscale, title='i = ' + str(i), 
+         show=show)
+    
+def examine(X, n=5, xlim=None, ylim=None):
+    '''randomly show some example in feature and label'''
+    n_X = len(X)
+    view_list = np.random.choice(n_X, min(n, n_X), replace=False)
+    for i in view_list:
+        viewi(X, i, xlim=xlim, ylim=ylim)
+    return view_list
+
+def comp_view(x1, x2, title=None, show=True):
+    plt.plot(x1)
+    plt.plot(x2)
+    if title: plt.title(title)
+    if show: plt.show()
+
+def comp_examine(data1, data2, view_n=10, view_list=None):
+    n_data = len(data1)
+    if not data1.shape == data2.shape: print(data1.shape, data2.shape, 
+                                             'not the same length')
+    if not view_list: view_list = np.random.choice(n_data, min(view_n, n_data), 
+                                                   replace=False)
+    for i in view_list:
+        comp_view(data1[i], data2[i], title='i = ' + str(i))
+    return list(view_list)
+
+def multi_view(x_list, title=None, show=True, xlim=None, ylim=None, 
+               yscale='linear'):
+    plt.yscale(yscale)
+    for x in x_list:
+        plt.plot(x)
+    if ylim: plt.ylim(ylim)
+    if xlim: plt.xlim(xlim)
+    if title: plt.title(title)
+    if show: plt.show()
+
+def examine_list(data_list, view_n=10, view_list=None, xlim=None, ylim=None, 
+                 yscale='linear'):
+    n_data = len(data_list[0])
+    if not view_list: view_list = np.random.choice(n_data, min(view_n, n_data), 
+                                                   replace=False)
+    for i in view_list:
+        multi_view([data[i] for data in data_list], 
+                   title='i = ' + str(i), xlim=xlim, ylim=ylim, yscale=yscale)
+    return list(view_list)
+
+def examine_list_gp(data_list, data_gp, view_n=10, view_list=None, xlim=None, 
+                    ylim=None, yscale='linear'):
+    n_data = len(data_list[0])
+    if not view_list: view_list = np.random.choice(n_data, min(view_n, n_data), 
+                                                   replace=False)
+    for i in view_list:
+        multi_view([data[i] for data in data_list], 
+                   title='i = ' + str(i) + ' gp = ' + str(data_gp[i]), 
+                   xlim=xlim, ylim=ylim, yscale=yscale)
+    return list(view_list)
+
+view_list = None
+
+# Array Information
+def arr_info(arr_list):
+    print('shape, min, max, median, stdev')
+    for arr in arr_list:
+        print(arr.shape, arr.min(), arr.max(), np.median(arr), arr.std())
